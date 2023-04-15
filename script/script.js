d2449358240a883a04b299dc0308e1f95b1ded81
Mari <wourtex99@gmail.com>
2023-04-15 12:16:37 +0300

    fix: update function changeBTN

--- script/script.js
+++ script/script.js
@@ -2,7 +2,24 @@ import petsJson from './pets.json' assert { type: 'json' };
 // console.log(petsJson[0].name);
 
 // slider
+// console.log(petsJson);
+const cardsWrapper = document.querySelector(".cards__wrapper");
+const BTN = document.querySelectorAll(".ribbon");
+const BTN_RIGHT = document.querySelector('.ribbon-right');
+const BTN_LEFT = document.querySelector('.ribbon-left');
+const SECTION_ACTIVE = document.querySelector(".cards__active");
+const SECTION_LEFT = document.querySelector(".cards__left");
+const SECTION_RIGHT = document.querySelector(".cards__right");
+
 let countCards = 3;
+
+/* функция перемешивания */
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 /* функция внутренней структуры карточки */
 function createCard(path, name) {
@@ -58,6 +75,53 @@ function renderCards (parent, pets = []){
   return arrayPets;
 }
 
+/* Адаптив блока с карточками */
+function resizeWindow() {
+  if(!resizeTimeout) {
+      resizeTimeout = setTimeout(() => {
+          resizeTimeout = null;
+          if (window.innerWidth < 1100) numberOfCards = 2;
+          if (window.innerWidth < 767) numberOfCards = 1;
+      }, 100);
+  }
+}
+
+/* обработка кликов */
+function changeBTN(event){
+  if(event.target.classList.contains("ribbon-left")){
+    // console.log("left btn");
+    SECTION_LEFT.innerHTML = '';
+    const newArray = renderCards(SECTION_LEFT);
+    cardsWrapper.classList.add("move-right");
+   
+    cardsWrapper.addEventListener("animationend", () => {
+      cardsWrapper.classList.remove("move-right");
+      SECTION_ACTIVE.innerHTML = '';
+      renderCards(SECTION_ACTIVE, newArray);
+    })
+  } else {
+    // console.log("right btn");
+    SECTION_RIGHT.innerHTML = '';
+    const newArray = renderCards(SECTION_RIGHT);
+    // console.log(newArray);
+    cardsWrapper.classList.add("move-left");
+
+    cardsWrapper.addEventListener("animationend", () => {
+      cardsWrapper.classList.remove("move-left");
+      SECTION_ACTIVE.innerHTML = '';
+      renderCards(SECTION_ACTIVE, newArray);
+    });
+  }
+  shuffle(petsJson);
+}
+
+/* event */
+BTN.forEach((btn) => btn.addEventListener("click", changeBTN));
 
 /* вызов функций */
+shuffle(petsJson);
+renderCards(document.querySelector(".cards__left"));
 renderCards(document.querySelector(".cards__active"));
+renderCards(document.querySelector(".cards__right"));
+window.addEventListener('resize', () => resizeWindow, false);
+let resizeTimeout;
