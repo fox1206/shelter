62cb915fda711770af805084a5f431979286b216
Mari <wourtex99@gmail.com>
2023-04-16 16:40:11 +0300

    feat: create function resizeWindow

--- script/scriptPage2.js
+++ script/scriptPage2.js
@@ -6,23 +6,37 @@ const BTN_PAGE = document.querySelectorAll(".btn__page");
 const arrayPets = [...petsJson, ...petsJson, ...petsJson, ...petsJson, ...petsJson, ...petsJson, ...petsJson];
 // console.log(arrayPets);
 
-let countCardPage = 8;
+let countCards = 8;
 let pageNumber = 1;
 let arrayPages = [];
 
-if(window.innerWidth < 1280) { countCardPage = 6; }
-if(window.innerWidth < 767) { countCardPage = 3; }
+if (window.innerWidth < 1250) countCards = 6;
+if (window.innerWidth < 650) countCards = 3;
+createPetsBox(countCards);
 
-createPetsBox();
-
+/* адаптив для отрисовки и получения числа карточек*/
+function resizeWindow() {
+  if(!resizeTimeout) {
+      resizeTimeout = setTimeout(() => {
+          resizeTimeout = null;
+          if (window.innerWidth > 1280) countCards = 8;
+          if (window.innerWidth < 1250) countCards = 6;
+          if (window.innerWidth < 650) countCards = 3;
+          if (window.innerWidth > 650 && pageNumber > arrayPages.length) pageNumber = arrayPages.length;
+          if (window.innerWidth > 1280 && pageNumber > arrayPages.length) pageNumber = arrayPages.length;
+          if (window.innerWidth <= 1250 && pageNumber < arrayPages.length) pageNumber = arrayPages.length;
+          if (window.innerWidth < 650 && pageNumber < arrayPages.length) pageNumber = arrayPages.length;
+      }, 100);
+  }
+}
 
 /* cоздать бокс со всеми карточками питомцев */
-function createPetsBox(){
+function createPetsBox(countCards){
   const PETS_BOX = document.querySelector(".pets__box");
   arrayPages = [];
 
-  for(let i = 0; i < 48; i += countCardPage){
-    let arr = arrayPets.slice(i, i + countCardPage).sort(() => {
+  for(let i = 0; i < 48; i += countCards){
+    let arr = arrayPets.slice(i, i + countCards).sort(() => {
       Math.random() - 0.5;
     });
     // console.log(arr);
@@ -36,7 +50,7 @@ function createPetsBox(){
     cardItem.className = "card__item";
 
     const cardContent = `<div class="card__picture">
-                          <img src="${arrayPages[0][i].img}" alt=${arrayPages[0][i].name}></img>
+                          <img src="${arrayPages[0][i].img}" alt="${arrayPages[0][i].name}"></img>
                         </div>
                         <div class="card__title">
                           <p>${arrayPages[0][i].name}</p>
@@ -49,3 +63,5 @@ function createPetsBox(){
   }
 }
 
+window.addEventListener('resize', resizeWindow, false);
+let resizeTimeout;
