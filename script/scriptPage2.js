70a95b69336049cb913e2b5166f7d83083c3c296
Mari <wourtex99@gmail.com>
2023-04-21 17:39:29 +0300

    feat: create popap and fix style for popap

--- script/scriptPage2.js
+++ script/scriptPage2.js
@@ -47,12 +47,17 @@ function createCards(name, img){
                    </div>`;
 
   petBox.appendChild(card);
-  return card;                   
+ 
+  // console.log(card.children[1].textContent);        
 }
 
 /* получить данные */
 function getCard(){
   let pets = random();
+
+  const MODAL_WRAPPER = document.querySelector(".modal__wrapper");
+  const MODAL_CONTENT = document.querySelector(".modal__content");
+  const MODAL_CLOSE = document.querySelector(".modal__close");
 
   let j = 0;
 
@@ -60,7 +65,27 @@ function getCard(){
     j++;
     createCards(pets[0][i].name, pets[0][i].img);
   }
-  console.log(pets);
+
+  document.querySelectorAll("body .card__item a").forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      
+      MODAL_CONTENT.innerHTML = createModalPets(pets[0][i].img, pets[0][i].name, pets[0][i].breed, pets[0][i].description, pets[0][i].age, pets[0][i].inoculations, pets[0][i].diseases, pets[0][i].parasites);
+      MODAL_WRAPPER.classList.add("show");
+      document.body.classList.add('modal');
+    });
+  });
+
+  MODAL_WRAPPER.classList.remove("show");
+  document.body.classList.remove('modal');
+
+  
+
+  MODAL_CLOSE.addEventListener("click", () => {
+    MODAL_WRAPPER.classList.remove("show");
+    document.body.classList.remove('modal');
+  });
+ 
+  // console.log(pets[0]);  
 }
 getCard();
 
@@ -128,6 +153,101 @@ function clickBtn(event){
   }
 }
 
+// попап
+function createModalPets(img, name, breed, description, age, inoculations, diseases, parasites){
+  return `
+            <div class="modal__picture">
+              <img src="${img}" alt="${name}"/>
+            </div>
+
+            <div class="modal__info">
+              <h3>${name}</h3>
+              <h4>${breed}</h4>
+              <p>${description}</p>
+                <ul>
+                  <li><span>Age: </span>${age}</li>
+                  <li><span>Inoculations: </span>${inoculations}</li>
+                  <li><span>Diseases: </span>${diseases}</li>
+                  <li><span>Parasites: </span>${parasites}</li>
+                </ul>
+            </div> 
+         `;
+}
+  
+
+
+
+
+
+
+
+
+
+
+/* 
+
+function createModalPets(){
+const MODAL_WRAPPER = document.querySelector(".modal__wrapper");
+const MODAL_CONTENT = document.querySelector(".modal__content");
+const MODAL_CLOSE = document.querySelector(".modal__close");
+const MORE_BTN = document.querySelectorAll('.card__item .card__btn a');
+
+//  console.log("card pets");
+let divPicture = document.createElement("div");
+let divInfo = document.createElement("div");
+  
+divPicture.className = "modal__picture";
+divInfo.className = "modal__info";
+
+divPicture.innerHTML = `<img src="" alt="1212" />`
+    divInfo.innerHTML = `
+                          <h3>1212</h3>
+                          <h4>122</h4>
+                          <p>1221</p>
+                          <ul>
+                            <li><span>Age: </span>121211</li>
+                            <li><span>Inoculations: </span>12</li>
+                            <li><span>Diseases: </span>1212</li>
+                            <li><span>Parasites: </span>12</li>
+                          </ul>
+                        `;
+
+    MODAL_CONTENT.append(divPicture);
+    MODAL_CONTENT.append(divInfo);
+
+    MODAL_WRAPPER.classList.add("show");
+    document.body.classList.add('modal');
+
+// MORE_BTN.forEach((btn, i) => {
+//   btn.addEventListener("click", () => {
+    
+
+//     divPicture.innerHTML = `<img src="" alt="1212" />`
+//     divInfo.innerHTML = `
+//                           <h3>1212</h3>
+//                           <h4>122</h4>
+//                           <p>1221</p>
+//                           <ul>
+//                             <li><span>Age: </span>121211</li>
+//                             <li><span>Inoculations: </span>12</li>
+//                             <li><span>Diseases: </span>1212</li>
+//                             <li><span>Parasites: </span>12</li>
+//                           </ul>
+//                         `;
+
+//     MODAL_CONTENT.append(divPicture);
+//     MODAL_CONTENT.append(divInfo);
+
+//     MODAL_WRAPPER.classList.add("show");
+//     document.body.classList.add('modal');
+//   });
+// });
+
+  // MODAL_CLOSE.addEventListener("click", () => {
+  //   MODAL_WRAPPER.classList.remove("show");
+  //   document.body.classList.remove('modal');
+  // });
+} */
 
 /* событие по изменению окна браузера */
 window.addEventListener("resize", resizeWin, false);
